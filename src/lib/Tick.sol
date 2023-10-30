4f272f53baf4dc4abb29b1fd68e9113e471ba8d3
hanbin <hnbn707@gmail.com>
2023-10-31 00:38:26 +0900

    setup foundry & 0. constructor

---
+++ src/lib/Tick.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.19;
+
+library Tick {
+    function tickSpacingToMaxLiquidityPerTick(
+        int24 tickSpacing
+    ) internal pure returns (uint128) {}
+}
